3199a8e846ebb8f7fdc225ffb9cc8c3a5d4fbd83
santoshxshrestha <santoshxshrestha@gmail.com>
2026-03-13 21:08:56 +0545

    init

--- flake.nix
+++ flake.nix
@@ -12,26 +12,10 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
       naerskLib = pkgs.callPackage naersk { };
-
-      runTimeDeps = [
-        pkgs.libxkbcommon
-
-        # GPU backend
-        pkgs.vulkan-loader
-        pkgs.libGL
-
-        # Window system
-        pkgs.wayland
-        pkgs.libx11
-        pkgs.libxcursor
-        pkgs.libxi
-        pkgs.zenity
-      ];
     in
     {
       packages.${system}.default = naerskLib.buildPackage {
         src = ./.;
-        buildInputs = [ pkgs.openssl ] ++ runTimeDeps;
         nativeBuildInputs = [ pkgs.pkg-config ];
       };
       devShells.${system}.default = pkgs.mkShell {
@@ -40,19 +24,9 @@
           pkgs.cargo
           pkgs.clippy
           pkgs.rustfmt
-          pkgs.openssl
-          pkgs.cargo-watch
           pkgs.rust-analyzer
-          # GUI dialog tool for build notifications
-          pkgs.zenity
         ];
-        nativeBuildInputs = [ pkgs.pkg-config ];
 
-        # env.RUST_SRC_PATH =
-        #   "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-        # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runTimeDeps;
-        env.RUSTFLAGS = "-C link-arg=-Wl,-rpath,${nixpkgs.lib.makeLibraryPath runTimeDeps}";
       };
       formatter = pkgs.rustfmt;
     };
